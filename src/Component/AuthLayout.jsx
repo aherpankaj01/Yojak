3d1e73d2ccfa01929adb57966f79966f3437563f
Aher Pankaj <aherpankaj22@gmail.com>
2026-05-29 21:52:37 +0530

    Optimize

--- src/Component/AuthLayout.jsx
+++ src/Component/AuthLayout.jsx
@@ -1,21 +1,28 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 export default function Protected({ children, authentication = true }) {
   const navigate = useNavigate();
-
+  const [loader, setLoader] = useState(true);
   const authStatus = useSelector((state) => state.auth.status);
 
   useEffect(() => {
     if (authentication && authStatus !== authentication) {
       navigate("/login");
-    }
-
-    if (!authentication && authStatus !== authentication) {
+    } else if (!authentication && authStatus !== authentication) {
       navigate("/");
     }
+    setLoader(false);
   }, [authStatus, navigate, authentication]);
 
-  return <>{children}</>;
+  return loader ? (
+    <div className="min-h-[60vh] flex items-center justify-center px-4">
+      <h1 className="text-sm sm:text-base md:text-lg text-gray-300 animate-pulse text-center">
+        Loading...
+      </h1>
+    </div>
+  ) : (
+    <>{children}</>
+  );
 }
